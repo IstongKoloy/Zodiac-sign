0231eecaa32ebc82c83a8471019a5d0329cf381b
Glenn Andaleon <andaleon34@gmail.com>
2023-06-05 18:36:56 +0800

    Update zodiacs page

--- zodiac-sign/src/components/pages/zodiac/Zodiac.jsx
+++ zodiac-sign/src/components/pages/zodiac/Zodiac.jsx
@@ -21,17 +21,51 @@ const Zodiac = () => {
   const fetchData = async () => {
     try {
       const response = await axios.request(options);
-      setData(response.data);
-      console.log(data)
+      const data = Object.keys(response.data).map((key) => ({
+        name: key,
+        ...response.data[key],
+      }));
+      setData(data);
     } catch (error) {
       console.error(error);
     }
   };
   
+  const [selectedZodiac, setSelectedZodiac] = useState(null);
+  const handleViewMore = (zodiac) => {
+    setSelectedZodiac(zodiac);
+  };
+  const handleBack = () => {
+    setSelectedZodiac(null);
+  };
 
   return (
-    <div className='container'>
-      
+    // <div className='container card'>
+    //     {data.map((data) => (
+    //     <div key={data.name} className='zodiacs-container'>
+    //       <h2>{data.name}</h2>
+    //       <p>Date: {data.date}</p>
+    //       <button>View more</button>
+    //     </div>
+    //   ))}
+    // </div>
+    <div className='card container'>
+      {selectedZodiac ? (
+        <div>
+          <h2>{selectedZodiac.name}</h2>
+          <p>Date: {selectedZodiac.date}</p>
+          <p>Personality: {selectedZodiac.personality}</p>
+          <button className='btn-viewmore' onClick={handleBack}>Back</button>
+        </div>
+      ) : (
+        data.map((data) => (
+          <div key={data.name} className='zodiacs-container'>
+            <h2>{data.name}</h2>
+            <p>Date: {data.date}</p>
+            <button className='btn-viewmore' onClick={() => handleViewMore(data)}>View More</button>
+          </div>
+        ))
+      )}
     </div>
   )
 }
